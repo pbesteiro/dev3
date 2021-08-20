ffc283c411fdd2157219bd5104f608df662910f2
Rodrigo Ayala <rodrigo@fmed.uba.ar>
2021-08-20 20:12:49 -0300

    first commit

---
+++ devvv-main/DevWeb/DevPlace/Negocio/ContactFormData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CruceroDelNorte.Negocio
+{
+    public class ContactFormData
+    {
+
+        public string Provinces { get; set; }
+        public string knowUsBy { get; set; }
+        public string Curse { get; set; }
+        public string Fee { get; set; }
+    }
+}
